d3a7f20ee5428e02b4420be5931a68eb95ccca7c
digitalcloudassets <info@digitalcloudassets.com>
2025-08-06 22:55:53 -0800

    Remove AI Chatbot from Kutable

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import TermsOfServicePage from './pages/TermsOfServicePage';
 import ForgotPassword from './components/Auth/ForgotPassword';
 import ResetPassword from './components/Auth/ResetPassword';
-import AIChatWidget from './components/Chat/AIChatWidget';
 
 // Set up global error handling
 setupGlobalErrorHandling();
@@ -142,8 +141,6 @@ function App() {
           }}
         />
         
-        {/* AI Chat Widget */}
-        <AIChatWidget />
       </div>
     </Router>
   );
