044a2c9b8e28bbee9d0d1a3a63b87cffe47223ea
digitalcloudassets <info@digitalcloudassets.com>
2025-08-09 22:46:58 -0800

    Remove bouncing mouse indicator from hero section

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -129,15 +129,6 @@ const Hero: React.FC = () => {
           </div>
         </div>
       </div>
-
-      {/* Features Preview */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-        <div className="animate-bounce">
-          <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"></div>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
