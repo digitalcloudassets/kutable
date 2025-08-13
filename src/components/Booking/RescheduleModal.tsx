bda2d9dbf11679b69f035ddf19d3972784b10abd
digitalcloudassets <info@digitalcloudassets.com>
2025-08-12 21:40:06 -0800

    Updated RescheduleModal.tsx

--- src/components/Booking/RescheduleModal.tsx
+++ src/components/Booking/RescheduleModal.tsx
@@ -86,6 +86,13 @@ const RescheduleModal: React.FC<RescheduleModalProps> = ({
     setSelectedDate(date);
     setStep('time');
   };
+
+  const handleTimeSelect = (time: string) => {
+    setSelectedTime(time);
+    setStep('confirm');
+  };
+
+  const handleConfirmReschedule = async () => {
     if (!selectedTime || !selectedDate) return;
 
     setLoading(true);
