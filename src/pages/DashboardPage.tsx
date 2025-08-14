5dfe99dd9da5f3d5af4119706ab09dec29f95e4a
digitalcloudassets <info@digitalcloudassets.com>
2025-08-14 13:19:14 -0800

    Fix admin debug panel visibility

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -386,6 +386,8 @@ const DashboardPage: React.FC = () => {
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-28">
           <SupabaseConnectionBanner isConnected={isConnected} />
           
+          <AdminDebugPanel />
+          
           <BarberDashboardHeader 
             barber={barber}
             onEditProfile={handleEditProfile}
